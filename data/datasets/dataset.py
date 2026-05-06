0eaac0fa9c178af3f907b0b7e13711cbb2b419bf
Matúš Moravčík <xmorav48@stud.fit.vut.cz>
2026-05-06 19:51:00 +0200

    feat: invert mask

--- data/datasets/dataset.py
+++ data/datasets/dataset.py
@@ -79,7 +79,7 @@ class MISRDataset(Dataset):
 
         # Split LR images and Mask
         lr_hsv = full_tensor[:, :3, :, :]
-        lr_masks = full_tensor[:, 3, :, :] / 255.0
+        lr_masks = full_tensor[:, 3, :, :] < 128
 
         return self._normalize_hsv(lr_hsv), self._normalize_hsv(target_tensor), lr_masks
 
