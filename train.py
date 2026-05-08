aa15a9a64504db786dd20eace088f65ff0a40621
Hai Phong Nguyen <xnguye28@merlin.fit.vutbr.cz>
2026-05-08 20:45:53 +0200

    fix: add fixed seeds to train and eval for reproducibility, add debug mode in train

--- train.py
+++ train.py
@@ -1,10 +1,17 @@
+import random
+
 import hydra
+import numpy as np
 from accelerate import Accelerator
 from omegaconf import DictConfig
+from torch.utils.data import Subset
 from torchinfo import summary
 
 from loggers.base_logger import BaseLogger
 from trainers import BasicTrainer as Trainer
+
+random.seed(42)
+np.random.seed(42)
 
 
 @hydra.main(config_path="configs", config_name="train", version_base=None)
@@ -20,6 +27,11 @@ def main(cfg: DictConfig) -> None:
 
     train_dataset = hydra.utils.instantiate(cfg.train_dataset)
     val_dataset = hydra.utils.instantiate(cfg.val_dataset)
+
+    if cfg.debug:
+        indices = range(min(len(train_dataset), cfg.num_debug_samples))
+        train_dataset = Subset(train_dataset, indices)
+        val_dataset = Subset(val_dataset, indices)
 
     train_loader = hydra.utils.instantiate(cfg.dataloader, dataset=train_dataset)
     val_loader = hydra.utils.instantiate(cfg.dataloader, dataset=val_dataset)
